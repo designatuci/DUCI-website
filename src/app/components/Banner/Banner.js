b27711335f2f6d9c32282fbac2d927f40f7f7cf8
Taesung Hwang <taesungh@uci.edu>
2023-04-30 19:27:37 -0700

    Include page banner about board recruitment
    - Temporarily use z-index since home hero graphic is position absolute
      - We should eventually simplify the layout to avoid z-index mess

---
+++ src/app/components/Banner/Banner.js
@@ -0,0 +1,19 @@
+import cn from './Banner.module.scss';
+
+const APPLICATION_FORM = 'https://forms.gle/e8zAUSGniWopQDNP9';
+
+function Banner() {
+	return (
+		<div className={cn.banner}>
+			<p>
+				Interested in becoming part of Design at UCI&apos;s Board? Submit a{' '}
+				<a className='color ultraviolet' href={APPLICATION_FORM}>
+					board application
+				</a>{' '}
+				by May 7th.
+			</p>
+		</div>
+	);
+}
+
+export default Banner;
--- src/app/components/index.js
+++ src/app/components/index.js
@@ -3,5 +3,6 @@ import Footer from './Footer';
 import Nav from './Nav';
 import Text from './Text';
 import WordBreak from './WordBreak';
+export { default as Banner } from './Banner';
 
 export { EmailForm, Footer, Nav, Text, WordBreak };
