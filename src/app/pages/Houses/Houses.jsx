3c6ed439129717cc476b051611b61d31102917ec
Ryan Yang <ryanqyang@gmail.com>
2023-10-02 21:52:38 -0700

    Force-pushing straight to master here just to get the house pages in

---
+++ src/app/pages/Houses/Houses.jsx
@@ -0,0 +1,311 @@
+import React, { memo, useCallback, useState } from "react";
+import cn from "./Houses.module.scss";
+import Helmet from "react-helmet";
+import { Icon, Section } from "app/Symbols";
+import { Text } from "app/components";
+import { Link } from "react-router-dom";
+import clsx from "clsx";
+import Symbol1 from "./assets/1.png";
+import Symbol2 from "./assets/2.png";
+import Symbol3 from "./assets/3.png";
+import Symbol4 from "./assets/4.png";
+import Symbol5 from "./assets/5.png";
+import Symbol6 from "./assets/6.png";
+import Symbol7 from "./assets/7.png";
+import Symbol8 from "./assets/8.png";
+import icon1 from "./assets/icon1.png";
+import icon2 from "./assets/icon2.png";
+import icon3 from "./assets/icon3.png";
+import icon4 from "./assets/icon4.png";
+import House1 from "./assets/Group1.png";
+import House2 from "./assets/Group2.png";
+import House3 from "./assets/Group3.png";
+import House4 from "./assets/Group4.png";
+
+export const Houses = memo(() => {
+  const HOUSES = [
+    {
+      name: "Water Tribe",
+      icon: House1,
+      description: "peaceful and resilient, this group presents calm and flow",
+      link: "https://docs.google.com/spreadsheets/d/1bEw6QacTrd6lTbqqx3D1CfJ068MuCs8cQRncch7Z_00",
+    },
+    {
+      name: "Earth Kingdom",
+      icon: House2,
+      description: "A diverse and resourceful nation known for its resolve",
+      link: "https://docs.google.com/spreadsheets/d/1GYwUszVwXia8AHADNwLRsiOQJvziCoSoblxnd1hR5AU",
+    },
+    {
+      name: "Fire Nation",
+      icon: House3,
+      description: "A powerful and ambitious empire ruled by determination",
+      link: "https://docs.google.com/spreadsheets/d/1ELynayIT6gn9DgyMRca5MmKwklLmVLqlM1-ofZdOx10",
+    },
+    {
+      name: "Air Nomads",
+      icon: House4,
+      description: "A nomadic society driven by a drive for peace",
+      link: "https://docs.google.com/spreadsheets/d/1ZOgMm07NGX_-DrO8enfEvCBsmawohOqziiyjTZSEn40",
+    },
+  ];
+
+  const HEADER_SYMBOLS = [
+    {
+      image: Symbol1,
+      top: 10,
+      left: 10,
+    },
+    {
+      image: Symbol2,
+      top: 10,
+      left: 80,
+    },
+    {
+      image: Symbol3,
+      top: 25,
+      left: 25,
+    },
+    {
+      image: Symbol4,
+      top: 30,
+      left: 88,
+    },
+    {
+      image: Symbol5,
+      top: 55,
+      left: 65,
+    },
+    {
+      image: Symbol6,
+      top: 60,
+      left: 15,
+    },
+    {
+      image: Symbol7,
+      top: 70,
+      left: 30,
+    },
+    {
+      image: Symbol8,
+      top: 70,
+      left: 70,
+    },
+  ];
+
+  return (
+    <main className={cn.container}>
+      <Helmet>
+        <title>Events – Design at UCI</title>
+      </Helmet>
+      <header>
+        {HEADER_SYMBOLS.map(({ image, top, left }, i) => (
+          <div
+            key={i}
+            className={cn.decoration}
+            style={{ top: top + "%", left: left + "%" }}
+          >
+            <img src={image} className={"wait"} />
+          </div>
+        ))}
+        <div className={clsx(cn.catch, "wait")}>
+          <Text size="M">
+            <span>WIN A PRIZE</span> • earn the most points
+          </Text>
+        </div>
+        <Text size="XXXL" className={clsx("wait", cn.title)}>
+          Avatar Houses
+        </Text>
+        <Text color="gray" className="wait">
+          be a part of your own design family
+        </Text>
+        <div className="wait show drop d10 l2">
+          <Link to="/join/" className={clsx("button L fill blue", cn.join)}>
+            <Text size="L" color="white" icon="right">
+              join now
+            </Text>
+          </Link>
+        </div>
+      </header>
+      <div className={cn.about} id="about">
+        <Section wrapperClass={cn.wrapper}>
+          <Text color="white" size="XXL" className={clsx(cn.title, "wait")}>
+            Network with other designers and compete for a prize
+          </Text>
+          <Text color="gray" size="L" className={clsx(cn.description, "wait")}>
+            Connect with a smaller group of designers, cultivate your industry
+            skills, and compete against other houses by attending our general
+            meetings, industry events, workshop events, and socials. The house
+            with the most points at the end of the quarter will receive a prize!
+          </Text>
+        </Section>
+      </div>
+      <div className={clsx(cn.houses, "wait")} id="houses">
+        <Section>
+          <Text size="XXL" className={cn.title}>
+            The Houses
+          </Text>
+          <div className={cn.list}>
+            {HOUSES.map(({ name, icon, description, link }) => (
+              <div key={name} className={clsx(cn.house, "wait")}>
+                <img src={icon} />
+                <Text size="L">{name}</Text>
+                <Text color="gray">
+                  {description}
+                  <br />
+                  <a
+                    style={{ marginTop: 8 }}
+                    href={link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    View Points
+                  </a>
+                </Text>
+              </div>
+            ))}
+          </div>
+          <div className="wait show drop d10 l2">
+            <Link to="/join/" className={clsx("button L fill blue", cn.join)}>
+              <Text size="L" color="white" icon="right">
+                view all points
+              </Text>
+            </Link>
+          </div>
+        </Section>
+      </div>
+      <div className={cn.activities} id="activities-and-perks">
+        <Section>
+          <Text color="white" size="XXL" className="wait">
+            Activities and Perks
+          </Text>
+          <div className={cn.list}>
+            <div className={clsx(cn.item, "wait")}>
+              <img src={icon1} />
+              <Text size="L" color="white">
+                Networking Opportunities
+              </Text>
+              <Text color="gray">
+                Connect with fellow designers and peers of all different
+                experience levels
+              </Text>
+            </div>
+            <div className={clsx(cn.item, "wait")}>
+              <img src={icon2} />
+              <Text color="white">
+                Hands-on Projects in Smaller Environments
+              </Text>
+              <Text color="gray">
+                Participate in design projects in concentrated team environments
+              </Text>
+            </div>
+            <div className={clsx(cn.item, "wait")}>
+              <img src={icon3} />
+              <Text color="white"> Win Prizes by attending events</Text>
+              <Text color="gray">
+                Participate in more activities and boost up house points to win
+                a prize!
+              </Text>
+            </div>
+            <div className={clsx(cn.item, "wait")}>
+              <img src={icon4} />
+              <Text color="white">
+                Personalized 1:1 Communication with Board
+              </Text>
+              <Text color="gray">
+                Talk directly with board members and schedule coffee chats
+              </Text>
+            </div>
+          </div>
+        </Section>
+      </div>
+      {/* <div className={cn.faq} id="FAQ">
+        <Section>
+          {[
+            {
+              q: "Where do I submit my project?",
+              a: (
+                <Text className="color gray">
+                  You will submit your final, working prototype to the{" "}
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://forms.gle/Tf1FySVor1RC5rHq8"
+                    style={{
+                      textDecoration: "underline",
+                    }}
+                  >
+                    Submission Form
+                  </a>{" "}
+                  (opens during event). Only one person on your team should
+                  submit a form, which will be on behalf of all contributors.
+                </Text>
+              ),
+            },
+          ].map((item) => {
+            return (
+              <Toggle
+                closed={
+                  <div>
+                    <h3>{item.q}</h3>
+                  </div>
+                }
+                opened={
+                  <div>
+                    <h3>{item.q}</h3>
+                    <Text
+                      className="color gray"
+                      style={{
+                        padding: "20px 0 0 34px",
+                      }}
+                    >
+                      {item.a}
+                    </Text>
+                  </div>
+                }
+              />
+            );
+          })}
+        </Section>
+      </div> */}
+      <section className={cn.bottom}>
+        <Text size="XXXL" className={cn.title}>
+          Join a tribe now
+        </Text>
+        <div className={clsx("wait show drop d10 l2", cn.button)}>
+          <Link to="/join/" className={clsx("button L fill blue", cn.join)}>
+            <Text size="L" color="white" icon="right">
+              join now
+            </Text>
+          </Link>
+        </div>
+      </section>
+    </main>
+  );
+});
+
+const Toggle = ({ opened, closed }) => {
+  const [open, setOpen] = useState(false);
+
+  const toggleExpand = useCallback(() => {
+    setOpen((p) => !p);
+  }, []);
+
+  return (
+    <div
+      className={clsx(cn.question, open && cn.opened)}
+      onClick={toggleExpand}
+    >
+      <Icon
+        src="d22-arrow.svg"
+        w="18"
+        h="18"
+        style={{
+          marginRight: "16px",
+          transform: open ? "rotate(180deg)" : "rotate(0deg)",
+        }}
+      />
+      {open ? opened : closed}
+    </div>
+  );
+};
--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -1,30 +1,32 @@
-import About from './About';
-import Brand from './Brand';
-import Contact from './Contact';
-import { Events, EventsAll } from './Events';
-import Home from './Home';
-import Join from './Join';
-import Mentorship from './Mentorship';
-import Merch from './Merch';
-import NotFound from './NotFound';
-import PT from './PT';
-import Resources from './Resources';
-import ResourcesFeatured from './ResourcesFeatured';
-import { Shirt22 } from './Merch/pages';
+import About from "./About";
+import Brand from "./Brand";
+import Contact from "./Contact";
+import { Events, EventsAll } from "./Events";
+import Home from "./Home";
+import Join from "./Join";
+import Mentorship from "./Mentorship";
+import Merch from "./Merch";
+import NotFound from "./NotFound";
+import PT from "./PT";
+import Resources from "./Resources";
+import ResourcesFeatured from "./ResourcesFeatured";
+import { Shirt22 } from "./Merch/pages";
+import { Houses } from "./Houses/Houses";
 
 export {
-	About,
-	Brand,
-	Contact,
-	Events,
-	EventsAll,
-	Home,
-	Join,
-	Mentorship,
-	Merch,
-	NotFound,
-	PT,
-	Resources,
-	ResourcesFeatured,
-	Shirt22,
+  About,
+  Brand,
+  Contact,
+  Events,
+  EventsAll,
+  Home,
+  Join,
+  Mentorship,
+  Merch,
+  NotFound,
+  PT,
+  Resources,
+  ResourcesFeatured,
+  Shirt22,
+  Houses,
 };
