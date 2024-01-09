d502cd5eebfeb71d436bac8b040e6cfc8df48e95
Taesung Hwang <taesungh@uci.edu>
2024-01-09 12:55:38 -0800

    Update House theme icons on Landing
    - Use new icons for Greek Gods houses with adaptable SVGs
    - Icons in standings still need to be updated

--- src/app/pages/Houses/sections/Landing.js
+++ src/app/pages/Houses/sections/Landing.js
@@ -1,55 +1,52 @@
 import clsx from "clsx";
 
 import { Text } from "app/components";
-import Symbol1 from "app/pages/Houses/assets/1.png";
-import Symbol2 from "app/pages/Houses/assets/2.png";
-import Symbol3 from "app/pages/Houses/assets/3.png";
-import Symbol4 from "app/pages/Houses/assets/4.png";
-import Symbol5 from "app/pages/Houses/assets/5.png";
-import Symbol6 from "app/pages/Houses/assets/6.png";
-import Symbol7 from "app/pages/Houses/assets/7.png";
-import Symbol8 from "app/pages/Houses/assets/8.png";
+
+import Bow from "app/pages/Houses/assets/Bow";
+import Dove from "app/pages/Houses/assets/Dove";
+import Flame from "app/pages/Houses/assets/Flame";
+import Lightning from "app/pages/Houses/assets/Lightning";
 
 import cn from "./Landing.module.scss";
 
 const HEADER_SYMBOLS = [
 	{
-		image: Symbol1,
+		icon: <Bow r={48} />,
 		top: 10,
 		left: 10,
 	},
 	{
-		image: Symbol2,
+		icon: <Lightning r={72} />,
 		top: 10,
 		left: 80,
 	},
 	{
-		image: Symbol3,
+		icon: <Dove gray r={48} opacity={0.35} />,
 		top: 25,
 		left: 25,
 	},
 	{
-		image: Symbol4,
+		icon: <Bow gray r={48} opacity={0.15} />,
 		top: 30,
 		left: 88,
 	},
 	{
-		image: Symbol5,
+		icon: <Flame gray r={40} opacity={0.5} />,
 		top: 55,
 		left: 65,
 	},
 	{
-		image: Symbol6,
+		icon: <Lightning gray r={40} opacity={0.25} />,
 		top: 60,
 		left: 15,
 	},
 	{
-		image: Symbol7,
+		icon: <Flame r={72} />,
 		top: 70,
 		left: 30,
 	},
 	{
-		image: Symbol8,
+		icon: <Dove r={56} />,
 		top: 70,
 		left: 70,
 	},
@@ -58,13 +55,13 @@ const HEADER_SYMBOLS = [
 function Landing({ scrollToJoin }) {
 	return (
 		<header className={cn.landing}>
-			{HEADER_SYMBOLS.map(({ image, top, left }, i) => (
+			{HEADER_SYMBOLS.map(({ icon, top, left }, i) => (
 				<div
 					key={i}
 					className={cn.decoration}
 					style={{ top: top + "%", left: left + "%" }}
 				>
-					<img alt="decorative" src={image} className={"wait"} />
+					{icon}
 				</div>
 			))}
 			<div className={clsx(cn.catch, "wait")}>
