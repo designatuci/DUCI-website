1dcf855edbaf1e6f5c8e3ae2156c4d3f54597cbe
Kevin Wu <kevinwu098@gmail.com>
2024-02-06 14:37:55 -0800

    Revert "kw/scaffold 2024 designathon (#52)" (#53)
    This reverts commit 278aff69c33346e3d0491422ab7b42ccc94e9a99.

--- src/app/pages/Designathons/Designathon24/components/index.js
+++
@@ -1,7 +0,0 @@
-export * from "./Profile";
-export * from "./Prizes";
-export * from "./SectionNavigation";
-export * from "./FAQ";
-export * from "./Rules";
-export * from "./Timer";
-export * from "./WinnerShowcase";
