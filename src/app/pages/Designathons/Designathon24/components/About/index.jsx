2acad4a63e00e7f17565f6c2b4749d7f4b3345fc
Kevin Wu <kevinwu098@gmail.com>
2024-03-06 11:37:17 -0800

    style: responsive paperclip

--- src/app/pages/Designathons/Designathon24/components/About/index.jsx
+++ src/app/pages/Designathons/Designathon24/components/About/index.jsx
@@ -1,6 +1,7 @@
 import cn from "./About.module.scss";
 import { useRef, useState, useEffect } from "react";
 import airplane from "../../assets/graphics/about/airplane.svg";
+import paperclip from "../../assets/graphics/about/paperclip.svg";
 
 const LINE_BREAKPOINTS = {
 	XL: 48,
@@ -100,6 +101,7 @@ const About = () => {
 			</div>
 
 			<img src={airplane} alt="airplane" className={cn.airplane} />
+			<img src={paperclip} alt="paperclip" className={cn.paperclip} />
 		</div>
 	);
 };
