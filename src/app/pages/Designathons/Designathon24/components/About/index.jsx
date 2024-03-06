79b7170448a33994bca6c9cd5acf8b3977657ee2
Kevin Wu <kevinwu098@gmail.com>
2024-03-06 12:24:54 -0800

    style: responsive top right stars

--- src/app/pages/Designathons/Designathon24/components/About/index.jsx
+++ src/app/pages/Designathons/Designathon24/components/About/index.jsx
@@ -4,6 +4,7 @@ import airplane from "../../assets/graphics/about/airplane.svg";
 import paperclip from "../../assets/graphics/about/paperclip.svg";
 import tr_gradient from "../../assets/graphics/about/tr_gradient.svg";
 import bl_gradient from "../../assets/graphics/about/bl_gradient.svg";
+import tr_stars from "../../assets/graphics/about/tr_stars.svg";
 
 const LINE_BREAKPOINTS = {
 	XL: 48,
@@ -106,6 +107,7 @@ const About = () => {
 			<img src={paperclip} alt="paperclip" className={cn.paperclip} />
 			<img src={tr_gradient} alt="tr_gradient" className={cn.tr_gradient} />
 			<img src={bl_gradient} alt="bl_gradient" className={cn.bl_gradient} />
+			<img src={tr_stars} alt="tr_stars" className={cn.tr_stars} />
 		</div>
 	);
 };
