8d8fe88ff791483a7a1b3f74e06c1665e6552d82
Kevin Wu <kevinwu098@gmail.com>
2024-03-05 19:55:01 -0800

    chore (personal): disable autoscroll on beta branches

--- src/app/controllers/useScroll.js
+++ src/app/controllers/useScroll.js
@@ -1,5 +1,5 @@
-import { useEffect, useCallback, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useEffect, useCallback, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 function useScroll() {
 	const [scrollableElements, setScrollableElements] = useState([]);
@@ -8,21 +8,23 @@ function useScroll() {
 	const pageScroll = useCallback(() => {
 		for (let element of scrollableElements) {
 			if (element.getBoundingClientRect().top < window.innerHeight * 0.84)
-				element.classList.add('show');
+				element.classList.add("show");
 		}
 	}, [scrollableElements]);
 
 	useEffect(() => {
+		if (pathname.includes("beta")) return; // makes life easier during dev
+
 		window.scrollTo({
-            top: 0,
-            left: 0,
-            behavior: 'smooth'
-        });
-		setScrollableElements(document.getElementsByClassName('wait'));
-		window.addEventListener('scroll', pageScroll);
+			top: 0,
+			left: 0,
+			behavior: "smooth",
+		});
+		setScrollableElements(document.getElementsByClassName("wait"));
+		window.addEventListener("scroll", pageScroll);
 		pageScroll();
-		
-		return () => window.removeEventListener('scroll', pageScroll);
+
+		return () => window.removeEventListener("scroll", pageScroll);
 	}, [pathname, pageScroll]);
 }
 
