1eef4a0416855ea2b1d5bc2c7cd9c630c09e9f25
Kevin Wu <kevinwu098@gmail.com>
2024-03-26 17:45:14 -0700

    chore: add null alt text to graphics

--- src/app/pages/Designathons/Designathon24/components/About/index.jsx
+++ src/app/pages/Designathons/Designathon24/components/About/index.jsx
@@ -15,42 +15,43 @@ const About = () => {
 			<h2 className={cn.aboutHeading}>About</h2>
 			<Notecard>
 				<p>
-					Design-a-thon is a 3 day long hybrid event where you or a team design
-					a product focused on the theme: community and inclusivity. It takes
-					place on February 24, starting at 5:30 pm and ends February 26, 8:00
-					pm PST.
+					Design-a-thon is a 3 day long hybrid event where you or a
+					team design a product focused on the theme: community and
+					inclusivity. It takes place on February 24, starting at 5:30
+					pm and ends February 26, 8:00 pm PST.
 				</p>
 				<p>
-					Design at UCI is the premier student-run organization at UCI for
-					anything graphic design, UI/UX design, product design, and more.
-					Started in 2016, we foster a special community for all digital
-					designers to connect, learn, and innovate.
+					Design at UCI is the premier student-run organization at UCI
+					for anything graphic design, UI/UX design, product design,
+					and more. Started in 2016, we foster a special community for
+					all digital designers to connect, learn, and innovate.
 				</p>
 				<p>
-					Design at UCI’s designathon is the largest collegiate designathon in
-					Orange County. Each year, we bring together hundreds of student
-					designers nationwide to define, develop, and pitch a product built
-					from scratch. We hope that this experience can help you acquire and
-					grow both your soft and hard skills in empathizing with your users,
-					defining a set of goals and needs, developing your product, and
-					improving your confidence and creativity as a human-centric designer.
+					Design at UCI’s designathon is the largest collegiate
+					designathon in Orange County. Each year, we bring together
+					hundreds of student designers nationwide to define, develop,
+					and pitch a product built from scratch. We hope that this
+					experience can help you acquire and grow both your soft and
+					hard skills in empathizing with your users, defining a set
+					of goals and needs, developing your product, and improving
+					your confidence and creativity as a human-centric designer.
 				</p>
 			</Notecard>
 
 			<img
 				src={airplane}
-				alt="airplane"
+				alt=""
 				className={clsx(cn.airplane, "wait flopR")}
 			/>
-			<img src={paperclip} alt="paperclip" className={cn.paperclip} />
+			<img src={paperclip} alt="" className={cn.paperclip} />
 			<img
 				src={tr_gradient}
 				alt="tr_gradient"
 				className={clsx(cn.tr, cn.gradient)}
 			/>
-			<img src={bl_gradient} alt="bl_gradient" className={cn.bl} />
-			<img src={tr_stars} alt="tr_stars" className={cn.tr} />
-			<img src={bl_stars} alt="bl_stars" className={cn.bl} />
+			<img src={bl_gradient} alt="" className={cn.bl} />
+			<img src={tr_stars} alt="" className={cn.tr} />
+			<img src={bl_stars} alt="" className={cn.bl} />
 		</div>
 	);
 };
