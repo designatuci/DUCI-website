1a8c34e547d9bec3e79cc8d12a900c3186cea35f
Kevin Wu <kevinwu098@gmail.com>
2024-03-27 03:12:00 -0700

    chore: move rules.json

--- src/app/pages/Designathons/Designathon24/Designathon24.js
+++ src/app/pages/Designathons/Designathon24/Designathon24.js
@@ -7,7 +7,6 @@ import JUDGES_2024 from "assets/data/designathon/2024/judges.json";
 import KEYNOTE_2024 from "assets/data/designathon/2024/keynote.json";
 import PRIZES_2024 from "assets/data/designathon/2024/prizes.json";
 import WORKSHOP_HOSTS_2024 from "assets/data/designathon/2024/workshop-hosts.json";
-import RULES_2024 from "assets/data/designathon/2024/rules.json";
 import cn from "./Designathon24.module.scss";
 import FOF from "./assets/FOF.png";
 import notion from "./assets/notion.png";
@@ -52,25 +51,26 @@ const Designathon24 = () => (
 			<Section>
 				<h2 className={cn.aboutHeading}>About</h2>
 				<p>
-					Design-a-thon is a 3 day long hybrid event where you or a team design
-					a product focused on the theme: community and inclusivity. It takes
-					place on February 24, starting at 5:30 pm and ends February 26, 8:00
-					pm PST.
+					Design-a-thon is a 3 day long hybrid event where you or a
+					team design a product focused on the theme: community and
+					inclusivity. It takes place on February 24, starting at 5:30
+					pm and ends February 26, 8:00 pm PST.
 				</p>
 				<p>
-					Design at UCI is the premier student-run organization at UCI for
-					anything graphic design, UI/UX design, product design, and more.
-					Started in 2016, we foster a special community for all digital
-					designers to connect, learn, and innovate.
+					Design at UCI is the premier student-run organization at UCI
+					for anything graphic design, UI/UX design, product design,
+					and more. Started in 2016, we foster a special community for
+					all digital designers to connect, learn, and innovate.
 				</p>
 				<p>
-					Design at UCI’s designathon is the largest collegiate designathon in
-					Orange County. Each year, we bring together hundreds of student
-					designers nationwide to define, develop, and pitch a product built
-					from scratch. We hope that this experience can help you acquire and
-					grow both your soft and hard skills in empathizing with your users,
-					defining a set of goals and needs, developing your product, and
-					improving your confidence and creativity as a human-centric designer.
+					Design at UCI’s designathon is the largest collegiate
+					designathon in Orange County. Each year, we bring together
+					hundreds of student designers nationwide to define, develop,
+					and pitch a product built from scratch. We hope that this
+					experience can help you acquire and grow both your soft and
+					hard skills in empathizing with your users, defining a set
+					of goals and needs, developing your product, and improving
+					your confidence and creativity as a human-centric designer.
 				</p>
 			</Section>
 		</div>
@@ -98,7 +98,7 @@ const Designathon24 = () => (
 			</Section>
 		</div>
 
-		<Rules rules={RULES_2024} />
+		<Rules />
 
 		<div className={cn.prizes} id="s-prizes">
 			<Prizes list={PRIZES_2024} />
