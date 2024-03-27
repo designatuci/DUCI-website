1255a8d1b7a3f50ef1998728609a4adc2e075c41
Kevin Wu <kevinwu098@gmail.com>
2024-03-27 02:49:55 -0700

    style: simplify speakers graphic styles

--- src/app/pages/Designathons/Designathon24/components/Speakers/index.jsx
+++ src/app/pages/Designathons/Designathon24/components/Speakers/index.jsx
@@ -4,10 +4,9 @@ import clsx from "clsx";
 import Polaroid from "../Polaroid/Polaroid";
 
 import splat from "../../assets/graphics/speakers/splat.svg";
-import pencil from "../../assets/graphics/speakers/pencil.svg";
+import tr_pencil_star_stripe from "../../assets/graphics/speakers/tr_pencil_star_stripe.svg";
 import tl_gradient from "../../assets/graphics/speakers/tl_gradient.svg";
 import tl_star_stripe from "../../assets/graphics/speakers/tl_star_stripe.svg";
-import tr_star_stripe from "../../assets/graphics/speakers/tr_star_stripe.svg";
 import ml_stripe from "../../assets/graphics/speakers/ml_stripe.svg";
 import mr_star_stripe from "../../assets/graphics/speakers/mr_star_stripe.svg";
 import br_star_stripe_gradient from "../../assets/graphics/speakers/br_star_stripe_gradient.svg";
@@ -67,31 +66,26 @@ const Speakers = () => {
 				</div>
 			</div>
 
-			<img
-				src={pencil}
-				alt=""
-				className={clsx(cn.pencil, "wait flopR")}
-			/>
 			<img src={tl_gradient} alt="" className={cn.tl_gradient} />
 			<img
 				src={tl_star_stripe}
 				alt=""
-				className={`${cn.tl_star_stripe} wait flopL`}
+				className={clsx(cn.tl_star_stripe, "wait flopL")}
 			/>
 			<img
-				src={tr_star_stripe}
+				src={tr_pencil_star_stripe}
 				alt=""
-				className={`${cn.tr_star_stripe} wait flopR`}
+				className={clsx(cn.tr_pencil_star_stripe, "wait flopR")}
 			/>
 			<img
 				src={ml_stripe}
 				alt=""
-				className={`${cn.ml_stripe} wait flopL`}
+				className={clsx(cn.m, cn.stripe, "wait flopL")}
 			/>
 			<img
 				src={mr_star_stripe}
 				alt=""
-				className={`${cn.mr_star_stripe} wait flopL`}
+				className={clsx(cn.m, cn.star_stripe, "wait flopR")}
 			/>
 			<img
 				src={br_star_stripe_gradient}
