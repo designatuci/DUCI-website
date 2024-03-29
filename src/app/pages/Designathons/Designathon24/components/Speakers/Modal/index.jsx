9414219ee7db1319e5c665a3291b14f93c21e96e
Kevin Wu <kevinwu098@gmail.com>
2024-03-29 15:46:47 -0700

    chore: cleanup present and past data

--- src/app/pages/Designathons/Designathon24/components/Speakers/Modal/index.jsx
+++ src/app/pages/Designathons/Designathon24/components/Speakers/Modal/index.jsx
@@ -1,29 +1,19 @@
 import clsx from "clsx";
 import cn from "./Modal.module.scss";
 
-export const Modal = ({
-	toggleProfile,
-	photo,
-	name,
-	role,
-	about,
-	funFact,
-	pronouns,
-}) => {
+export const Modal = ({ toggleProfile, photo, name, role, about }) => {
 	return (
 		<div className={clsx(cn.modal)} onClick={toggleProfile}>
 			<div className={cn.container}>
 				<img src={require(`../../../assets/${photo}`)} alt={name} />
 				<div className={cn.profile}>
 					<h3>{name}</h3>
-					<p className={cn.pronouns}>{pronouns}</p>
 					<p className={cn.role}>{role}</p>
 				</div>
 				<div className={cn.content}>
 					<h4>About Me: </h4>
 					<p>{about}</p>
 					<h4>Fun Fact: </h4>
-					<p>{funFact}</p>
 				</div>
 			</div>
 		</div>
