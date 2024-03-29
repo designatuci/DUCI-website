7f491902cf9204b30c9a3400912311cbb39d733f
Kevin Wu <kevinwu098@gmail.com>
2024-03-29 15:49:39 -0700

    feat: listen for esc key

--- src/app/pages/Designathons/Designathon24/components/Speakers/Modal/index.jsx
+++ src/app/pages/Designathons/Designathon24/components/Speakers/Modal/index.jsx
@@ -1,7 +1,23 @@
 import clsx from "clsx";
 import cn from "./Modal.module.scss";
 
+import { useEffect } from "react";
+
 export const Modal = ({ toggleProfile, photo, name, role, about }) => {
+	useEffect(() => {
+		const handleKeyDown = (event) => {
+			if (event.key === "Escape") {
+				toggleProfile();
+			}
+		};
+
+		document.addEventListener("keydown", handleKeyDown);
+
+		return () => {
+			document.removeEventListener("keydown", handleKeyDown);
+		};
+	}, [toggleProfile]);
+
 	return (
 		<div className={clsx(cn.modal)} onClick={toggleProfile}>
 			<div className={cn.container}>
