69dd57e61c2d44f7c50a5c6dc9c123fc64efe7e7
Ryan Yang <ryanqyang@gmail.com>
2024-04-07 15:00:24 -0700

    putting in some placeholders

--- src/app/pages/Designathons/Designathon24/components/Splash/Splash.jsx
+++ src/app/pages/Designathons/Designathon24/components/Splash/Splash.jsx
@@ -19,6 +19,33 @@ const Splash = memo(() => {
 
 	return (
 		<div className={cn.container}>
+			<div className={cn.info}>
+				<div className={cn.time}>
+					May 17th 6:30 PM - May 19th 4:30 PM
+				</div>
+				<div className={cn.wrapper}>
+					<div className={cn.border}>
+						<a
+							className={cn.app}
+							target="_blank"
+							rel="noreferrer noopener"
+							href="https://docs.google.com/forms/d/1dt6fiBAjGVi4M4PO3V2ZUkieRHR6fn6wWvC5E6RZoZM"
+						>
+							Apply as Designer
+						</a>
+					</div>
+					<div className={cn.border}>
+						<a
+							className={cn.app}
+							href="https://docs.google.com/forms/d/11YEH02IGBs800naIoY8gpi_dZ5hrH15_1OXlBKNnuAs"
+							target="_blank"
+							rel="noreferrer noopener"
+						>
+							Apply as Mentor
+						</a>
+					</div>
+				</div>
+			</div>
 			<SplashLogo ref={logoRef} className={cn.logo} />
 		</div>
 	);
