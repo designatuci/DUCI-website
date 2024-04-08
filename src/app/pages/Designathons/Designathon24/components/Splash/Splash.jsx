fc35b699d82c442c18855510602b9491f09ecdaa
Kevin Wu <kevinwu098@gmail.com>
2024-04-08 16:30:58 -0700

    feat: add graphics

--- src/app/pages/Designathons/Designathon24/components/Splash/Splash.jsx
+++ src/app/pages/Designathons/Designathon24/components/Splash/Splash.jsx
@@ -1,7 +1,16 @@
 import { memo, useEffect, useRef } from "react";
 import { ReactComponent as SplashLogo } from "./logo.svg";
 import cn from "./Splash.module.scss";
+import clsx from "clsx";
 import anime from "animejs";
+
+import tl_clouds from "../../assets/graphics/splash/clouds.svg";
+import butterfly from "../../assets/graphics/splash/butterfly.svg";
+import bl_star_stripe from "../../assets/graphics/splash/bl_star_stripe.svg";
+import mr_cloud_stars from "../../assets/graphics/splash/mr_cloud_stars.svg";
+import br_star_stripe from "../../assets/graphics/splash/br_star_stripe.svg";
+
+import GridBackground from "../Backgrounds/GridBackground";
 
 const Splash = memo(() => {
 	const logoRef = useRef(null);
@@ -19,6 +28,54 @@ const Splash = memo(() => {
 
 	return (
 		<div className={cn.container}>
+			<img
+				src={tl_clouds}
+				alt=""
+				className={clsx(
+					cn.decoration,
+					cn.tl_clouds,
+					"wait flopL_des24",
+				)}
+			/>
+			<img
+				src={butterfly}
+				alt=""
+				className={clsx(
+					cn.decoration,
+					cn.butterfly,
+					"wait flopL_des24",
+				)}
+			/>
+			<img
+				src={bl_star_stripe}
+				alt=""
+				className={clsx(
+					cn.decoration,
+					cn.bl_star_stripe,
+					"wait flopL_des24",
+				)}
+			/>
+			<img
+				src={mr_cloud_stars}
+				alt=""
+				className={clsx(
+					cn.decoration,
+					cn.mr_cloud_stars,
+					"wait flopR_des24",
+				)}
+			/>
+			<img
+				src={br_star_stripe}
+				alt=""
+				className={clsx(
+					cn.decoration,
+					cn.br_star_stripe,
+					"wait flopR_des24",
+				)}
+			/>
+
+			<GridBackground positions={[{ top: 0, left: 0 }]} isLight={true} />
+
 			<div className={cn.info}>
 				<div className={cn.time}>
 					May 17th 6:30 PM - May 19th 4:30 PM
