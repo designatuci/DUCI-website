485ed874f07f7ecdfcd9dd076a6574435848d5ae
Kevin Wu <kevinwu098@gmail.com>
2024-04-08 11:37:16 -0700

    feat: in person prizes

--- src/app/pages/Designathons/Designathon24/components/Prizes/index.jsx
+++ src/app/pages/Designathons/Designathon24/components/Prizes/index.jsx
@@ -10,41 +10,23 @@ const Prizes = ({ list }) => {
 		<div id="s-prizes">
 			<div className={cn.container}>
 				<GridBackground
-					positions={[{ top: 0, left: 0 }]}
+					positions={[
+						{ top: 0, left: 0 },
+						{ bottom: 0, right: 0 },
+					]}
 					isLight={true}
 				/>
 
 				<div className={cn.content}>
 					<h2 className={cn.heading}>Prizes</h2>
 
-					<h3 className={cn.prizeSectionHeading}>Top 3</h3>
+					<div className={cn.prizeSectionContainer}>
+						<h3 className={cn.prizeSectionHeading}>Top 3</h3>
 
-					<div className={clsx(cn.prizeContainer, cn.first)}>
-						<h6 className={cn.prizeHeader}>1st Place</h6>
-
-						<div className={cn.prizes}>
-							<p>$100 Visa Card</p>
-							<p>
-								1:1 Mentorship/Career Discussion with judge of
-								your choice
-							</p>
-							<p>
-								Feature on Design at UCI Website & Design at UCI
-								Instagram
-							</p>
-						</div>
-
-						<img src={trophy} alt="" className={cn.trophy} />
-					</div>
-
-					<div className={cn.secondThirdContainer}>
-						<div
-							className={clsx(cn.prizeContainer, cn.secondThird)}
-						>
-							<h6 className={cn.prizeHeader}>2nd Place</h6>
-
+						<div className={clsx(cn.prizeContainer, cn.first)}>
+							<h6 className={cn.prizeHeader}>1st Place</h6>
 							<div className={cn.prizes}>
-								<p>$50 Visa Card</p>
+								<p>$100 Visa Card</p>
 								<p>
 									1:1 Mentorship/Career Discussion with judge
 									of your choice
@@ -54,23 +36,92 @@ const Prizes = ({ list }) => {
 									UCI Instagram
 								</p>
 							</div>
+							<img src={trophy} alt="" className={cn.trophy} />
 						</div>
+						<div className={cn.secondThirdContainer}>
+							<div
+								className={clsx(
+									cn.prizeContainer,
+									cn.secondThird,
+								)}
+							>
+								<h6 className={cn.prizeHeader}>2nd Place</h6>
 
-						<div
-							className={clsx(cn.prizeContainer, cn.secondThird)}
-						>
-							<h6 className={cn.prizeHeader}>3rd Place</h6>
+								<div className={cn.prizes}>
+									<p>$50 Visa Card</p>
+									<p>
+										1:1 Mentorship/Career Discussion with
+										judge of your choice
+									</p>
+									<p>
+										Feature on Design at UCI Website &
+										Design at UCI Instagram
+									</p>
+								</div>
+							</div>
+							<div
+								className={clsx(
+									cn.prizeContainer,
+									cn.secondThird,
+								)}
+							>
+								<h6 className={cn.prizeHeader}>3rd Place</h6>
+								<div className={cn.prizes}>
+									<p>$25 Visa Card</p>
+									<p>
+										1:1 Mentorship/Career Discussion with
+										judge of your choice
+									</p>
+									<p>
+										Feature on Design at UCI Website &
+										Design at UCI Instagram
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
 
-							<div className={cn.prizes}>
-								<p>$25 Visa Card</p>
-								<p>
-									1:1 Mentorship/Career Discussion with judge
-									of your choice
-								</p>
-								<p>
-									Feature on Design at UCI Website & Design at
-									UCI Instagram
-								</p>
+					<div className={cn.prizeSectionContainer}>
+						<h3 className={cn.prizeSectionHeading}>
+							In-Person Prizes
+						</h3>
+
+						<div className={cn.inpersonContainer}>
+							<div
+								className={clsx(cn.prizeContainer, cn.inperson)}
+							>
+								<div>
+									<h6 className={cn.prizeHeader}>
+										People's Choice
+									</h6>
+									<p className={cn.prizeDetails}>
+										Voted by in-person participants.
+									</p>
+								</div>
+
+								<div className={cn.prizes}>
+									<p>Lego Sets</p>
+									<p>Figma Pins</p>
+									<p>Design at UCI Stickers</p>
+								</div>
+							</div>
+							<div
+								className={clsx(cn.prizeContainer, cn.inperson)}
+							>
+								<div>
+									<h6 className={cn.prizeHeader}>
+										Honorable Mentions
+									</h6>
+									<p className={cn.prizeDetails}>
+										Voted by in-person participants.
+									</p>
+								</div>
+
+								<div className={cn.prizes}>
+									<p>Lego Sets</p>
+									<p>Figma Pins</p>
+									<p>Design at UCI Stickers</p>
+								</div>
 							</div>
 						</div>
 					</div>
