16dadb7eece243c13e9887e7991e21f80b785b0b
Kevin Wu <kevinwu098@gmail.com>
2024-05-08 22:18:36 -0700

    chore: remove signup buttons (#116)

--- src/app/pages/Designathons/Designathon24/components/Splash/Splash.jsx
+++ src/app/pages/Designathons/Designathon24/components/Splash/Splash.jsx
@@ -14,38 +14,6 @@ const Splash = memo(() => {
 
 			<div className={clsx(cn.info, "wait")}>
 				<div className={cn.time}>May 17th - May 19th</div>
-				<div className={cn.wrapper}>
-					<div className={cn.border}>
-						<a
-							className={cn.app}
-							target="_blank"
-							rel="noreferrer noopener"
-							href="https://docs.google.com/forms/d/1dt6fiBAjGVi4M4PO3V2ZUkieRHR6fn6wWvC5E6RZoZM"
-						>
-							Apply as Designer
-						</a>
-					</div>
-					<div className={cn.border}>
-						<a
-							className={cn.app}
-							target="_blank"
-							rel="noreferrer noopener"
-							href="https://forms.gle/bPGtCSx8VfZoGXjW6"
-						>
-							Apply as Mentor
-						</a>
-					</div>
-					<div className={cn.border}>
-						<a
-							className={cn.app}
-							target="_blank"
-							rel="noreferrer noopener"
-							href="https://docs.google.com/forms/d/1g6CjdlOxet1fplwTCWQE8pZ-wKzHjku-t_a-bwRwK_Q/viewform"
-						>
-							Apply as Volunteer
-						</a>
-					</div>
-				</div>
 			</div>
 		</div>
 	);
